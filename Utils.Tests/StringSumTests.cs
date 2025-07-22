ecf82fca87d6debb62bc74e9bc2f94a5500447ed
bohdan zhylavskyi <zhylavskyibohdan@gmail.com>
2025-07-22 21:27:19 +0300

    [StringSum] add failing 'Sum_OneOrBothOperandsAreNonNatural_ReturnsCorrectSum' test

--- Utils.Tests/StringSumTests.cs
+++ Utils.Tests/StringSumTests.cs
@@ -20,5 +20,18 @@ namespace Utils.Tests
 
             Assert.Equal(expectedSum, actualResult);
         }
+
+        [Theory]
+        [InlineData("-10", "4", "4")]
+        [InlineData("5", "NAN", "5")]
+        [InlineData("NAN", "NAN", "0")]
+        [InlineData("10", "0.5", "10")]
+        [InlineData("", "30", "30")]
+        public void Sum_OneOrBothOperandsAreNonNatural_ReturnsCorrectSum(string num1, string num2, string expectedSum)
+        {
+            var actualResult = Utils.Sum(num1, num2);
+
+            Assert.Equal(expectedSum, actualResult);
+        }
     }
 }
