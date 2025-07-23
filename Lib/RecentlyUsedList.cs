70860cc971b6bf8c83070d005020982cfd3b00dd
bohdan zhylavskyi <zhylavskyibohdan@gmail.com>
2025-07-23 15:34:50 +0300

    [RecentlyUsedList] add failing 'RecentlyUsedList_AddSingleItem_ProcessedCorrectly' and 'RecentlyUsedList_AddMultipleItems_ProcessedCorrectly' tests

---
+++ Lib/RecentlyUsedList.cs
@@ -0,0 +1,17 @@
+﻿namespace Utils
+{
+    public class RecentlyUsedList
+    {
+        public void Add(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string this[int i]
+        {
+            get => throw new NotImplementedException();
+        }
+
+        public int Length => throw new NotImplementedException();
+    }
+}
---
+++ Utils.Tests/RecentlyUsedListTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Utils.Tests
+{
+    public class RecentlyUsedListTests
+    {
+        [Fact]
+        public void RecentlyUsedList_AddSingleItem_ProcessedCorrectly()
+        {
+            var list = new RecentlyUsedList();
+            var item = "item";
+
+            list.Add(item);
+
+            Assert.Equal(1, list.Length);
+            Assert.Equal(list[0], item);
+        }
+
+        [Fact]
+        public void RecentlyUsedList_AddMultipleItems_ProcessedCorrectly()
+        {
+            var list = new RecentlyUsedList();
+            var item1 = "item 1";
+            var item2 = "item 2";
+
+            list.Add(item1);
+            list.Add(item2);
+
+            Assert.Equal(2, list.Length);
+            Assert.Equal(list[0], item2);
+            Assert.Equal(list[1], item1);
+        }
+    }
+}
