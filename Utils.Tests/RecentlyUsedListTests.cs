4d6571716224035f9ee069b33a20b1bea0dbb1b1
bohdan zhylavskyi <zhylavskyibohdan@gmail.com>
2025-07-23 16:19:09 +0300

    [RecentlyUsedList] add failing 'RecentlyUsedList_AddDuplicates_ListContainsOnlyUniqueItems' and 'RecentlyUsedList_AddDuplicates_DuplicatedItemMovedRatherThanAdded' tests

--- Utils.Tests/RecentlyUsedListTests.cs
+++ Utils.Tests/RecentlyUsedListTests.cs
@@ -28,5 +28,34 @@
             Assert.Equal(list[0], item2);
             Assert.Equal(list[1], item1);
         }
+
+        [Fact]
+        public void RecentlyUsedList_AddDuplicates_ListContainsOnlyUniqueItems()
+        {
+            var list = new RecentlyUsedList();
+            var item = "item";
+
+            list.Add(item);
+            list.Add(item);
+
+            Assert.Equal(1, list.Length);
+            Assert.Equal(list[0], item);
+        }
+
+        [Fact]
+        public void RecentlyUsedList_AddDuplicates_DuplicatedItemMovedRatherThanAdded()
+        {
+            var list = new RecentlyUsedList();
+            var item1 = "item 1";
+            var item2 = "item 2";
+
+            list.Add(item1);
+            list.Add(item2);
+            list.Add(item1);
+
+            Assert.Equal(2, list.Length);
+            Assert.Equal(list[0], item1);
+            Assert.Equal(list[1], item2);
+        }
     }
 }
